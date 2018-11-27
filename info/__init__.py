f641c6772a46afabb63454d3a015f17e518dd74e
hzw1010355837 <1010355837@qq.com>
2018-11-27 21:02:25 +0800

    创建app对象方法,并重新在manage对象调用

--- info/__init__.py
+++ info/__init__.py
@@ -2,17 +2,23 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf import CSRFProtect
 from redis import StrictRedis
-from config import Config
+from config import config_dict
 from flask_session import Session
 
-# 1创建app对象
-app = Flask(__name__)
-app.config.from_object(Config)
-# 2创建db对象
-db = SQLAlchemy(app)
-# 3创建redis_store对象
-redis_store = StrictRedis(host=Config.REDIS_HOST, port=Config.REDIS_PORT)
-# 4csrf包含请求体都需要csrf
-CSRFProtect(app)
-# 5Session将数据存放到redis中
-Session(app)
+db = SQLAlchemy()
+
+
+def create_app(config_name):
+    # 1创建app对象
+    app = Flask(__name__)
+    app.config.from_object(config_dict[config_name])
+    # 2创建db对象
+    # db = SQLAlchemy(app)
+    db.init_app(app)
+    # 3创建redis_store对象
+    redis_store = StrictRedis(host=config_dict[config_name].REDIS_HOST, port=config_dict[config_name].REDIS_PORT)
+    # 4csrf包含请求体都需要csrf
+    CSRFProtect(app)
+    # 5Session将数据存放到redis中
+    Session(app)
+    return app
--- manage.py
+++ manage.py
@@ -1,8 +1,9 @@
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
-from info import app, db
+from info import create_app, db
 
-
+# 1 创建app对象
+app = create_app("development")
 # 6migrate数据库迁移
 manage = Manager(app)
 migrate = Migrate(app, db)
